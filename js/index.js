5c65372792b58f988604417f3a040da74e20aacf
Kartikeya99 <kgkartikeyagokhale@gmail.com>
2018-05-02 16:58:50 +0530

    Added the quote slideshow

--- js/index.js
+++ js/index.js
@@ -12,9 +12,9 @@ $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() {
   $('#slideshow > div:first')
-    .fadeOut(1)
+    .fadeOut(1000)
     .next()
-    .fadeIn(1)
+    .fadeIn(1000)
     .end()
     .appendTo('#slideshow');
 }, 3000);
